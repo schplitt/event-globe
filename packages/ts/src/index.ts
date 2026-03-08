4d41a1620f165e275fd949ed1c9508b4be37f8c6
schplitt <schplitt.contact@gmail.com>
2026-03-08 21:40:37 +0100

    lint:fix

--- packages/ts/src/index.ts
+++ packages/ts/src/index.ts
@@ -1,13 +1,13 @@
 export * from './globe-renderer'
 export type {
-	GlobeConfig,
-	GlobeEventType,
-	GlobeEventFinishReason,
-	GlobeEventOptionsMap,
-	GlobeEvents,
-	GlobeEventOptions,
-	ArcEventOptions,
-	GlobeEventResult,
-	EventHandle,
-	ArcOptions,
+  GlobeConfig,
+  GlobeEventType,
+  GlobeEventFinishReason,
+  GlobeEventOptionsMap,
+  GlobeEvents,
+  GlobeEventOptions,
+  ArcEventOptions,
+  GlobeEventResult,
+  EventHandle,
+  ArcOptions,
 } from '@event-globe/core'
